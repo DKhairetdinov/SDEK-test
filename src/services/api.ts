d848aa1a05ff0853d67ab52323ac98e24c26ce14
DKhairetdinov <haretdinov.201@mail.ru>
2026-04-28 12:30:31 +0400

    feat: add news types and api request

---
+++ src/services/api.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+import type { ApiRecovery } from "../types/news";
+
+const API_BASE_URL = 'GET http://1e14c3489fcb.vps.myjino.ru:5000/api/v1/news/feed/company/';
+
+export const fetchNews = async (page: number, perPage: number, isEmpty: boolean = false): Promise<ApiRecovery> => {
+    const endpoint = isEmpty ? '/empty' : '/short';
+
+    const response = await axios.get<ApiRecovery>(`${API_BASE_URL}${endpoint}`, {
+        params: {
+            page,
+            perPage,
+        },
+    });
+
+    return response.data;
+}
---
+++ src/types/news.ts
@@ -0,0 +1,34 @@
+export interface NewsImageSize {
+    s: string;
+    m: string;
+    l: string;
+    hd: string;
+}
+
+export interface NewCover {
+    type: "gallery" | string;
+    image: NewsImageSize;
+}
+
+export interface NewsRubrics {
+    id: number;
+    slug: string;
+    name: string;
+}
+
+export interface NewsItem {
+    id: string;
+    title: string;
+    cover: NewCover | null;
+    likeCount: number;
+    viewCount: number;
+    publishedAt: string;
+    rubrics: NewsRubrics[];
+}
+
+export interface ApiRecovery {
+    totalPges: number;
+    perPage: number;
+    news: NewsItem[];
+    minDatePublication: string;
+}
